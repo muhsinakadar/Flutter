2550900b5f860f5d71048035dc57a2e8f7576f83
muhsinakadar <muhsinatk97@gmail.com>
2023-07-22 16:42:37 +0530

    Project Starting

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+main(){
+  runApp(app);
+}
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Container(
+
+    );
+  }
+}
